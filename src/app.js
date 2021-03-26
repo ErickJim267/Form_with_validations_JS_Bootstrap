0aacfb8aad07d66dd3b70e33a1857342d6ec146e
Rainfire267 <ejimenez267@gmail.com>
2021-03-26 15:49:18 +0000

    update

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 import "bootstrap";
 import "./style.css";
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {};
+
+let form = document.getElementById("miFormulario");
+
+form.addEventListener(
+  "submit",
+  function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    let comodin = true,
+      input = this.elements,
+      total = input.length;
+
+    for (let i = 0; i < total; i++) {
+      if (!input[i].value.length) {
+        alert("Alerta! Por favor revise sus respuestas");
+        input[i].focus();
+        comodin = false;
+        break;
+      }
+    }
+
+    if (comodin) {
+      this.submit();
+    }
+  },
+  false
+);
